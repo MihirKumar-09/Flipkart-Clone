54a0090285c5fd07d6fced1203b0b28b2a5a4d1b
MihirKumar <mihirmalik34@gmail.com>
2025-12-10 17:29:27 +0530

    fix the cancel button

--- Frontend/src/Components/PriceDrop/Button/Button.jsx
+++ Frontend/src/Components/PriceDrop/Button/Button.jsx
@@ -1,8 +1,11 @@
 import style from "./Button.module.css";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 export default function Button() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const product = location.state;
+  const id = product?._id;
   const notify = () => {
     toast.success("Alert Set Successfully", {
       autoClose: 2000,
@@ -16,7 +19,7 @@ export default function Button() {
   };
   return (
     <div className={style.button}>
-      <button>Cancel</button>
+      <button onClick={() => navigate(`/product/${id}`)}>Cancel</button>
       <button onClick={handleSetAlert}>Set Alert</button>
     </div>
   );
