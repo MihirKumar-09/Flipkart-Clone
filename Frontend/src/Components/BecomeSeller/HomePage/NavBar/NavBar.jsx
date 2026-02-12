ddf3911e8e411d9c2515fa687ea4957e571d58a2
MihirKumar <mihirmalik34@gmail.com>
2026-02-12 20:49:13 +0530

    Fix style

--- Frontend/src/Components/BecomeSeller/HomePage/NavBar/NavBar.jsx
+++ Frontend/src/Components/BecomeSeller/HomePage/NavBar/NavBar.jsx
@@ -1,47 +1,11 @@
 import style from "./NavBar.module.css";
-// Import Logo;
-import NavLogo from "../../../../assets/Seller/navLogo.svg";
+// import NavLogo from "../../../../assets/Seller/navLogo.svg";
+// import { useState } from "react";
+
 export default function NavBar() {
   return (
-    <nav className={style.navContainer}>
-      <div className={style.navLeft}>
-        <img src={NavLogo} alt="Seller Logo" />
-
-        <ul>
-          <li>
-            Sell Online
-            <span className="material-symbols-outlined">
-              keyboard_arrow_down
-            </span>
-          </li>
-          <li>
-            Fees and Commission
-            <span className="material-symbols-outlined">
-              keyboard_arrow_down
-            </span>
-          </li>
-          <li>
-            Grow
-            <span className="material-symbols-outlined">
-              keyboard_arrow_down
-            </span>
-          </li>
-          <li>
-            Learn
-            <span className="material-symbols-outlined">
-              keyboard_arrow_down
-            </span>
-          </li>
-          <li>Shopsy</li>
-        </ul>
-      </div>
-
-      <div className={style.navRight}>
-        <ul>
-          <li className={style.loginBtn}>Login</li>
-          <li className={style.sellingBtn}>Start Selling</li>
-        </ul>
-      </div>
-    </nav>
+    <div>
+      <h6>Nav Bar</h6>
+    </div>
   );
 }
