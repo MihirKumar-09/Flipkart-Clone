a9c42ca10a80d779fbe974c7174e24ef725f670e
MihirKumar <mihirmalik34@gmail.com>
2026-02-17 13:59:46 +0530

    Show not found page properly for wrong route request

--- Frontend/src/Components/NotFound/NotFound.jsx
+++ Frontend/src/Components/NotFound/NotFound.jsx
@@ -1,10 +1,26 @@
 import style from "./NotFound.module.css";
+import Logo from "../../assets/HomePage/webLogo.png";
+import LogoText from "../../assets/HomePage/LoginPageLogo.png";
+import NoFoundLogo from "../../assets/NoFoundPage/noFound.png";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>404</h1>
-      <h3>Not Found</h3>
+    <div className={style.noFoundContainer}>
+      <nav>
+        <span>
+          <img src={LogoText} alt="Flipkart" />
+          <img src={Logo} alt="Logo" />
+        </span>
+      </nav>
+      <div className={style.container}>
+        <img src={NoFoundLogo} alt="" />
+        <p>
+          Unfortunately the page you are looking for has been moved or deleted
+        </p>
+        <button onClick={() => navigate("/")}>GO TO HOME PAGE</button>
+      </div>
     </div>
   );
 }
