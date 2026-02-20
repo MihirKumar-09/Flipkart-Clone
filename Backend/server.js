e39051e79ac95d9dbb73497530c52024d533f9d5
MihirKumar <mihirmalik34@gmail.com>
2026-02-20 22:06:14 +0530

    Fix the cors

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: "https://flipkart-clone-1-eay9.onrender.com/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
